d9c1be1a20d87367a90451c5e0ad46c9788f8b79
adityadixit07 <adityadixit9660@gmail.com>
2024-02-18 13:04:08 +0530

    authorize

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,14 +1,12 @@
 import express from "express";
 import UserController from "../controllers/UserController.js";
 import authorize from "../utils/Authorize.js";
-import { User } from "../models/User.js";
 
 const UserRoutes = express.Router();
 
 UserRoutes.route("/register").post(UserController.register);
 UserRoutes.route("/login").post(UserController.login);
 
-// UserRoutes.route('/profile').get(authorize,UserController.profile)
 
 UserRoutes.route("/addcourse/:courseId").post(
   authorize,
